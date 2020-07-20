c07bb15ce88e0c980c8f8433c17baa092e72667b
max.lee <f820602h@gmail.com>
2020-07-20 14:25:59 +0800

    heroku

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const path = require('path')
 module.exports = {
-  // publicPath: '/',
+  publicPath: '/',
   lintOnSave: true,
   outputDir: path.resolve(__dirname, './server/dist'),
   chainWebpack: config => {
