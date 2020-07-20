d2091d458a31f8b34aaa1241bd7eca33473de4dd
max.lee <f820602h@gmail.com>
2020-07-20 12:45:54 +0800

    router mode

--- src/router/index.js
+++ src/router/index.js
@@ -34,7 +34,7 @@ const routes = [
 ]
 
 const router = new VueRouter({
-  mode: 'hash',
+  mode: process.env.NODE_ENV === 'production' ? 'history' : 'hash',
   base: process.env.BASE_URL,
   routes
 })
