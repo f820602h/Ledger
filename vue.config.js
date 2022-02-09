afdf589be92cba277f2b3e4e09c117da0db2dc69
max.lee <f820602h@gmail.com>
2022-02-09 14:35:30 +0800

    hot reload setting

--- vue.config.js
+++ vue.config.js
@@ -1,26 +1,24 @@
 const path = require("path");
-const webpack = require("webpack");
-const hmr = new webpack.HotModuleReplacementPlugin();
+// const webpack = require("webpack");
+// const hmr = new webpack.HotModuleReplacementPlugin();
 
 module.exports = {
   publicPath: "/",
   lintOnSave: true,
   outputDir: path.resolve(__dirname, "./server/dist"),
   chainWebpack: config => {
-    if (process.env.NODE_ENV !== "development") {
-      config
-        .entry("app")
-        .clear()
-        .add(path.join(__dirname, "./src/main.js"));
-    } else {
-      config
-        .entry("app")
-        .clear()
-        .add("webpack-hot-middleware/client?quiet=true")
-        .add("webpack/hot/only-dev-server")
-        .add(path.join(__dirname, "./src/main.js"));
-      config.plugin("hot").use(hmr);
-      config.entry.app = ["babel-polyfill", "./src/main.js"];
-    }
+    config
+      .entry("app")
+      .clear()
+      .add(path.join(__dirname, "./src/main.js"));
+
+    // config
+    //   .entry("app")
+    //   .clear()
+    //   .add("webpack-hot-middleware/client?quiet=true")
+    //   .add("webpack/hot/only-dev-server")
+    //   .add(path.join(__dirname, "./src/main.js"));
+    // config.plugin("hot").use(hmr);
+    // config.entry.app = ["babel-polyfill", "./src/main.js"];
   }
 };
