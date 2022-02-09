7dd2801a6b9ae782276dc0b4d56bd233b5ff144b
max.lee <f820602h@gmail.com>
2022-02-09 14:10:38 +0800

    hot reload setting

--- vue.config.js
+++ vue.config.js
@@ -7,8 +7,12 @@ module.exports = {
   lintOnSave: true,
   outputDir: path.resolve(__dirname, "./server/dist"),
   chainWebpack: config => {
-    config.entry.app = ["babel-polyfill", "./src/main.js"];
-    if (process.env.NODE_ENV !== "production") {
+    if (process.env.NODE_ENV === "production") {
+      config
+        .entry("app")
+        .clear()
+        .add(path.join(__dirname, "./src/main.js"));
+    } else {
       config
         .entry("app")
         .clear()
@@ -16,6 +20,7 @@ module.exports = {
         .add("webpack/hot/only-dev-server")
         .add(path.join(__dirname, "./src/main.js"));
       config.plugin("hot").use(hmr);
+      config.entry.app = ["babel-polyfill", "./src/main.js"];
     }
   }
 };
